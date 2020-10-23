6b3f6c04cac1e1147fa65aadf4f44e68f858c500
Satoshi Takimoto <wasabi.nvim.st@gmail.com>
2020-10-23 17:35:43 +0900

    Session

--- src/main/scala/sysdes/formapp/SessionServer.scala
+++ src/main/scala/sysdes/formapp/SessionServer.scala
@@ -2,32 +2,93 @@ package sysdes.formapp
 
 import java.net.Socket
 import java.util.UUID
+import scala.collection.mutable.HashMap
 import scala.io.Source
-import sysdes.formapp.server.{Handler, Server}
+import sysdes.formapp.server.{BadRequest, Handler, Interpolator, Server, UrlEncodedDecoder}
 
 object SessionServer extends Server(8002) {
   override def getHandler(socket: Socket) = new SessionServerHandler(socket)
 }
 
 object SessionServerHandler {
-  // インスタンス間で共有する内部状態に関する変数・関数はこの中に記述
+  val states: HashMap[UUID, State] = HashMap()
 }
+
+class State(var name: String = "", var gender: String = "", var message: String = "")
 
 class SessionServerHandler(socket: Socket) extends Handler(socket) {
   import sysdes.formapp.server.{NotFound, Ok, Request, Response}
 
-  def handle(request: Request): Response = request match {
-    case req if !req.headers.contains("Cookie") => index()
-    case Request("GET", "/", _, _, _) => index()
-    case _                            => NotFound(s"Requested resource '${request.path}' for ${request.method} is not found.")
+  def handle(request: Request): Response = {
+    if (!request.headers.contains("Cookie")) {
+      return index()
+    }
+
+    val sessId = UUID.fromString(request.headers("Cookie").stripPrefix("session-id="))
+    if (!SessionServerHandler.states.contains(sessId)) {
+      return BadRequest("400 Bad Request")
+    }
+
+    request match {
+      case Request("GET", p, _, _, _) if (p.takeWhile(_ != '?') == "/") => index()
+      case Request("POST", "/register-name", _, _, _) => nameForm()
+      case Request("POST", "/register-gender", _, _, Some(body)) => genderForm(sessId, body)
+      case Request("POST", "/register-message", _, _, Some(body)) => messageForm(sessId, body)
+      case Request("POST", "/summary", _, _, Some(body)) => summary(sessId, body)
+      case _                            => NotFound(s"Requested resource '${request.path}' for ${request.method} is not found.")
+    }
   }
 
   def index(): Response = {
     val src = Source.fromFile("./html/index.html")
     val html = try src.mkString finally src.close()
     val res = Ok(html)
-    val id = UUID.randomUUID()
-    res.addHeader("Set-Cookie", s"session-id=${id}")
+
+    val sessId = UUID.randomUUID()
+    SessionServerHandler.states.put(sessId, new State)
+    res.addHeader("Set-Cookie", s"session-id=${sessId}")
+
     res
+  }
+
+  def nameForm(): Response = {
+    val src = Source.fromFile("./html/nameForm.html")
+    val html = try src.mkString finally src.close()
+    Ok(html)
+  }
+
+  def genderForm(sessId: UUID, body: String): Response = {
+    val bodyMap = UrlEncodedDecoder.decode(body)
+    SessionServerHandler.states(sessId).name = bodyMap("name")
+
+    val src = Source.fromFile("./html/genderForm.html")
+    val html = try src.mkString finally src.close()
+    Ok(html)
+  }
+
+  def messageForm(sessId: UUID, body: String): Response = {
+    val bodyMap = UrlEncodedDecoder.decode(body)
+    SessionServerHandler.states(sessId).gender = bodyMap("gender")
+
+    val src = Source.fromFile("./html/messageForm.html")
+    val html = try src.mkString finally src.close()
+    Ok(html)
+  }
+
+  def summary(sessId: UUID, body: String): Response = {
+    val bodyMap = UrlEncodedDecoder.decode(body)
+    val state = SessionServerHandler.states(sessId)
+    state.message = bodyMap("message")
+
+    val src = Source.fromFile("./html/summary.html")
+    val html = try src.mkString finally src.close()
+    Ok(Interpolator.interpolate(
+      html,
+      Map(
+        "name" -> state.name,
+        "gender" -> state.gender,
+        "message" -> state.message,
+      )
+    ))
   }
 }
